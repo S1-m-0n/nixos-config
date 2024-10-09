79f779e1a9c77b99e0159f60ce20d396acde7bd3
Simon <sim.joerz@gmail.com>
2024-10-09 19:30:19 +0200

    draft of an example python dev-env flake

---
+++ python-dev-env/flake.nix
@@ -0,0 +1,37 @@
+{
+
+  description = "A python development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs, ...} @ input:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+
+      packages = with pkgs; ([
+        
+        python312
+  
+      ]) ++ (with pkgs.python312Packages; [
+
+        python312Packages.numpy
+
+      ]);
+
+      shellHook = ''
+        zsh
+      '';
+
+    };
+    
+  };
+
+}
