b089fe42ac9e81a72f82fbd4000dec9c9e1df796
Simon <sim.joerz@gmail.com>
2024-10-14 16:16:05 +0200

    removed fzf

--- nixosModules/neovim/config/lua/plugins/fzf.lua
+++ nixosModules/neovim/config/lua/plugins/fzf.lua
@@ -1,20 +1,20 @@
 return {
-  {
-    'nvim-telescope/telescope-fzf-native.nvim',
-    build = 'make',
-    requires = { 'nvim-telescope/telescope.nvim' },
-    config = function()
-      require('telescope').setup {
-        extensions = {
-          fzf = {
-            fuzzy = true,
-            overridw_generic_sorter = true,
-            override_file_orter = true,
-            case_mode = "smart_case",
-          }
-        }
-      }
-      require('telescope').load_extension('fzf')
-    end,
-  },
+  -- {
+  --   'nvim-telescope/telescope-fzf-native.nvim',
+  --   -- build = 'make',
+  --   requires = { 'nvim-telescope/telescope.nvim' },
+  --   config = function()
+  --     require('telescope').setup {
+  --       extensions = {
+  --         fzf = {
+  --           fuzzy = true,
+  --           overridw_generic_sorter = true,
+  --           override_file_orter = true,
+  --           case_mode = "smart_case",
+  --         }
+  --       }
+  --     }
+  --     require('telescope').load_extension('fzf')
+  --   end,
+  -- },
 }
