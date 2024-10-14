53a89ecaacdcee19c9cfb9cfb364c485e28caeae
Simon <sim.joerz@gmail.com>
2024-10-14 12:29:30 +0200

    moved nvim config

--- nixosModules/neovim/neovim.nix
+++ nixosModules/neovim/neovim.nix
@@ -1,11 +1,19 @@
-{ config, pkgs, unstable }:
+{ config, pkgs, unstable, ... }:
 
 {
   
   programs.neovim = {
     enable = true;
     defaultEditor = true;
-    extraConfig = lib.fileContents ./init.lua;
+    # extraConfig = pkgs.lib.fileContents ./init.lua;
+  };
+
+  # home.file."./init.lua".text = ''
+  # '';
+
+  home.file."./.config/nvim/" = {
+    source = ./config;
+    recursive = true;
   };
 
 }
