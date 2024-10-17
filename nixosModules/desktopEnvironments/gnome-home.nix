b1ff5e2010c64d640381f46fe15d4bb82f9d5265
Simon <sim.joerz@gmail.com>
2024-10-17 10:02:51 +0200

    added shortcuts to dconf

--- nixosModules/desktopEnvironments/gnome-home.nix
+++ nixosModules/desktopEnvironments/gnome-home.nix
@@ -18,7 +18,36 @@
       # sources = "[('xkb', 'de'), ('xkb', 'de+neo_qwertz')]";
       sources = [ (lib.gvariant.mkTuple [ "xkb" "de" ]) (lib.gvariant.mkTuple [ "xkb" "de+neo_qwertz" ]) ];
       xkb-options = [ "terminate:ctrl_alt_bksp" ];
+    };
 
+    # windows keybindings
+    "org/gnome/desktop/wm/keybindings" = {
+      close = [ "<Super>q" ];
+    };
+
+    # launcher keybindings
+    # "org/gnome/settings-daemon/plugins/media-keys" = {
+    #   www = [ "<Super>c" ];
+    # };
+
+    # media key bindings
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+
+      # keybinding for launching the browser
+      www = [ "<Super>b" ];
+
+      # custom keybindings are declared here
+      custom-keybindings = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal/"
+      ];
+
+    };
+
+    # custom keybinding to launch terminal is defined here
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal" = {
+      name = "Launch Terminal";
+      command = "kitty";
+      binding = "<Super>t";
     };
 
   };
