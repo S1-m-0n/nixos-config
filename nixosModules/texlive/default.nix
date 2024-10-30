7fb9fd7ebc9f124a943dd66a22c7eef1e9f6755b
Simon <sim.joerz@gmail.com>
2024-10-30 12:12:18 +0100

    changed a few details

--- nixosModules/texlive/default.nix
+++ nixosModules/texlive/default.nix
@@ -1,12 +1,23 @@
 # nixosModules/texlive/default.nix
 # home-manager configuration for texlive
 
+# interesting post: https://discourse.nixos.org/t/new-tex-live-withpackages-function/34902
+# may make it easier to include packages
+
+# it may be easier to use a dev-environment for bigger latex projects, then to install a big set
+# of packages, or even the full scheme
+
 { pkgs, ... }:
 
 let
   tex = (pkgs.texlive.combine {
     inherit (pkgs.texlive)
-    scheme-basic;
+    scheme-basic
+    # scheme-medium
+    # scheme-full
+    # svg   # include svg images
+    # tuda-ci urcls anyfontsize xcharter xstring fontaxes roboto adjustbox trimspaces   # tu-darmstadt templates
+    ;
   });
 in {
 
